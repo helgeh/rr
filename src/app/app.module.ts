e254831cad27e54634a6d7793b919d22100d917f
Helge Hofstad <helge.hofstad@gmail.com>
2016-11-05 00:27:54 +0100

    dryss på litt material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PodcastListComponent } from './podcast-list/podcast-list.component';
@@ -19,7 +20,8 @@ import { PodcastsService } from './podcasts.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    MaterialModule.forRoot()
   ],
   providers: [PodcastsService],
   bootstrap: [AppComponent]
