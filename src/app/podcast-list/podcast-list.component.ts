62a246a4a1280cf70335011d0ebcaa8733497951
Helge Hofstad <helge.hofstad@gmail.com>
2016-11-05 15:23:46 +0100

    vis ti og ti podkaster, onScroll

--- src/app/podcast-list/podcast-list.component.ts
+++ src/app/podcast-list/podcast-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { PodcastsService } from '../podcasts.service';
 
 @Component({
@@ -8,15 +9,34 @@ import { PodcastsService } from '../podcasts.service';
 })
 export class PodcastListComponent {
 
+  private allPodcastItems: Object[];
+  
   podcastItems :Object[];
+  cap: number = 1;
 
   constructor(private podcastsService: PodcastsService) { }
 
   ngOnInit() {
-    this.podcastsService.getItems().then(items => {
-      this.podcastItems = items;
-      console.log(items);
-    });
+    this.podcastsService.getItems()
+      .then(items => this.setItems(items));
+  }
+
+  private setItems(items: Object[]) {
+    this.allPodcastItems = items;
+    this.podcastItems = this.getClampedItems();
+  }
+
+  private getClampedItems(): Object[] {
+    return this.allPodcastItems.slice(0, this.cap * 10);
+  }
+
+  // TODO: This should be throttled somehow. Maybe by adding a "load more"" button?
+  onScroll(event) {
+    let reveal = (window.scrollY > window.document.body.clientHeight - window.innerHeight);
+    if (reveal) {
+      this.cap++;
+      this.podcastItems = this.getClampedItems();
+    }
   }
 
 }
